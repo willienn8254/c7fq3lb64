b74030192fc01ad32c569d4dffb3f542ed6dfcdb
willienn8254 <willienn@hotmail.com>
2016-01-29 23:22:40 -0500

    Agrega funcionalidad Buscar

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,15 @@
 class ExpensesController < ApplicationController
   def index
      @expenses = Expense.order("date DESC")
+
+     if params[:concept].present?
+      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+    end
+
+     if params[:category_id].present?
+
+     	@expenses=Expense.where("category_id = ?", params[:category_id])
+
+     end
   end
 end
